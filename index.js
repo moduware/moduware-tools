6c2f8079fc6e0b32d603aa7a88f58626804526fb
Alex Chernov <alexusblack@gmail.com>
2018-04-05 15:45:18 +1000

    making request to api

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let accessToken = null;
 
 document.addEventListener('DOMContentLoaded', async function() {
   document.getElementById('clientAuthoriseButton').addEventListener('click', clientAuthoriseButtonClickHandler);
+  document.getElementById('uuidListSubmit').addEventListener('click', uuidListSubmitClickHandler);
   
   const isAuthorised = await checkAuthorisation();
   if(!isAuthorised) {
@@ -16,15 +17,105 @@ document.addEventListener('DOMContentLoaded', async function() {
     const userNameField = document.getElementById('userName');
     userNameField.textContent = user.name;
     userNameField.classList.remove('d-none');
+    
+    await loadProductTypes();
   }
 });
 
+function clientAuthoriseButtonClickHandler(event) {
+  const redirectUri = document.location.href.replace(document.location.hash, '');
+  auth0.authorize({
+    audience: 'https://api.moduware.com',
+    scope: 'openid profile email admin',
+    responseType: 'token',
+    grant_type: 'client_credentials',
+    redirectUri: redirectUri
+  });
+}
+
+function uuidListSubmitClickHandler(event) {
+  document.getElementById('badUuidAlert').classList.add('d-none');
+  document.getElementById('uuidsSubmittedAlert').classList.add('d-none');
+
+  const category = document.getElementById('productCategorySelect').value;
+  const type = document.getElementById('productTypeSelect').value;
+  const uuidsText = document.getElementById('productUuidsList').value;
+  if(uuidsText == '') {
+    alert('UUIDs list cannot be empty');
+    return;
+  }
+  const uuids = uuidsText.split("\n");
+  const cleanUuids = [];
+
+  let badUuidFound = false;
+  for(let i = 0; i<uuids.length; i++) {
+    let uuid = uuids[i];
+    if(uuid == '') continue;
+    if(uuid.length != 32) {
+      badUuidFound = true;
+      showBadUuidError(i);
+      break;
+    }
+    cleanUuids.push(uuid);
+  }
+
+  if(badUuidFound) return;
+  console.log(category, type, cleanUuids);
+  document.getElementById('productAddForm').classList.add('d-none');
+  document.getElementById('productAddProgress').classList.remove('d-none');
+  submitUuids(category, type, cleanUuids);
+}
+
+async function submitUuids(category, type, uuids) {
+  let percent = 0;
+  let i = 0;
+  for(let uuid of uuids) {
+    let newPercent = parseInt(i / uuids.length * 100);
+    if(newPercent != percent) {
+      percent = newPercent;
+      showProgressPercent(percent);
+    }
+    // let result = await postData(`http://api.moduware.com/v1/product/${uuid}`, {
+    let result = await postData(`http://localhost:3000/v1/product/${uuid}`, {
+      type: type,
+      category: category
+    });
+    console.log(result);
+    return;
+    //await sleep(1000);
+    i++;
+  }
+
+  document.getElementById('productAddForm').classList.remove('d-none');
+  document.getElementById('productAddProgress').classList.add('d-none');
+  document.getElementById('uuidsSubmittedAlert').classList.remove('d-none');
+}
+
+function showProgressPercent(percent) {
+  const progressbar = document.getElementById('productAddProgressBar');
+  progressbar.textContent = percent + '%';
+  progressbar.style.width = percent + '%';
+}
+
+function sleep(milliseconds) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), milliseconds);
+  });
+}
+
+function showBadUuidError(line) {
+  document.getElementById('badUuidAlert').classList.remove('d-none');
+  document.getElementById('badUuidLineNumber').textContent = line;
+}
+
 function checkAuthorisation() {
   return new Promise((resolve, reject) => {
-    auth0.parseHash({ hash: window.location.hash }, function(err, authResult) {
+    auth0.parseHash({}, function(err, authResult) {
       if (err) {
         resolve(false);
         console.log(err);
+      } else if(authResult == null) {
+        resolve(false);
       } else {
         resolve(true);
         accessToken = authResult.accessToken;
@@ -39,6 +130,7 @@ function getUser() {
     auth0.client.userInfo(accessToken, function(err, user) {
       if(err) {
         console.log(err);
+        alert('Cannot retrieve use information');
         reject(err);
       } else {
         console.log('User: ', user);
@@ -48,14 +140,23 @@ function getUser() {
   });
 }
 
-function clientAuthoriseButtonClickHandler(event) {
-  const redirectUri = document.location.href.replace(document.location.hash, '');
-  auth0.authorize({
-    audience: 'https://api.moduware.com',
-    responseType: 'token',
-    grant_type: 'client_credentials',
-    redirectUri: redirectUri
+
+function getProductTypesList() {
+  try {
+    return fetch(`http://api.moduware.com/v1/product-type/list`).then(response => response.json());
+  } catch(err) {
+    console.log(err);
+    alert('Cannot retrieve product types list');
+  }
+}
+
+async function loadProductTypes() {
+  const list = await getProductTypesList();
+  let html = '';
+  list.map(type => {
+    html += `<option value="${type}">${type}</option>`;
   });
+  document.getElementById('productTypeSelect').innerHTML = html;
 }
 
 function postData(url, data) {
@@ -66,7 +167,8 @@ function postData(url, data) {
     credentials: 'same-origin', // include, same-origin, *omit
     headers: {
       'user-agent': 'Mozilla/4.0 MDN Example',
-      'content-type': 'application/json'
+      'content-type': 'application/json',
+      'Authorization': 'Bearer ' + accessToken
     },
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
     mode: 'cors', // no-cors, cors, *same-origin
